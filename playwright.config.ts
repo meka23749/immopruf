2ab494e68ccd868c347ad69be83fe3c1311d028f
Meka <meka23749@gmail.com>
2026-06-12 19:51:52 +0200

    fix: update workflow to Node.js 24 and fix report upload

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from '@playwright/test';
 
-const isCI = process.env.CI === 'true';
-
 export default defineConfig({
   testDir: './tests',
-  reporter: [['html'], ['list']],
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }], ['list']],
   use: {
-    baseURL: isCI
-      ? 'file:///home/runner/work/immopruf/immopruf/app/index.html'
-      : 'file:///C:/Users/Steve/Desktop/immopruf/app/index.html',
     headless: true,
   },
 });
